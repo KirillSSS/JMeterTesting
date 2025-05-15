193d7b97dadfbceea2e4d6b8b8fd7ff8f7ec2216
Кірілл Савчук <savchuk.kirill.ua@gmail.com>
2025-05-15 23:43:28 +0300

    Add bat file

---
+++ StartTestPlan.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Встанови шлях до JMeter (зміні на свій)
+set JMETER_PATH=""
+
+set TEST_PLAN=".\TestPlan.jmx"
+
+set RESULTS_FOLDER=".\tests\results"
+
+if not exist %RESULTS_FOLDER% mkdir %RESULTS_FOLDER%
+
+%JMETER_PATH%\jmeter.bat -n -t %TEST_PLAN% -l %RESULTS_FOLDER%\results.jtl -e -o %RESULTS_FOLDER%\dashboard
+
+pause
